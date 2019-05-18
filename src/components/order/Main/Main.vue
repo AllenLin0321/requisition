c9269851258d88827763c9195e85c2896e170ae2
Allen <glux7381@gmail.com>
2019-05-18 21:56:23 +0800

    Can't show the chinese work

--- src/components/order/Main/Main.vue
+++ src/components/order/Main/Main.vue
@@ -39,30 +39,28 @@ import { mapGetters } from "vuex";
 import jsPDF from "jspdf";
 import jspdfAutotable from "jspdf-autotable";
 
-
-
 export default {
   data() {
     return {
       todos: [
         {
-          title: 'title 1',
-          description: 'description 1'
+          title: "title 1",
+          description: "description 1"
         },
         {
-          title: 'title 2',
-          description: 'description 2'
+          title: "title 2",
+          description: "description 2"
         },
         {
-          title: 'title 3',
-          description: 'description 3'
+          title: "title 3",
+          description: "description 3"
         },
         {
-          title: 'title 4',
-          description: 'description 4'
-        },
+          title: "title 4",
+          description: "description 4"
+        }
       ]
-    }
+    };
   },
   computed: {
     ...mapGetters(["materials", "fileName"])
@@ -75,16 +73,16 @@ export default {
   methods: {
     downloadPDF() {
       let pdfName = this.fileName;
-      var doc = new jsPDF('p', 'pt');
+      var doc = new jsPDF("p", "pt");
 
       var columns = [
-        {title: "項目", dataKey: "item"},
-        {title: "數量", dataKey: 'quantity'},
-        {title: "單位", dataKey: 'unit'}
+        { title: "項目", dataKey: "item" },
+        { title: "數量", dataKey: "quantity" },
+        { title: "單位", dataKey: "unit" }
       ];
-      
-      doc.autoTable(columns, this.materials)
 
+
+      doc.autoTable(columns, this.materials,);
       doc.save(pdfName + ".pdf");
     }
   }
