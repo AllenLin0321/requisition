aa3e2b7dd89d31149ce0f1d6cde32f1fa9e36905
Allen <glux7381@gmail.com>
2019-05-23 11:57:07 +0800

    Finish Add Main Catelog

--- src/components/label/LabelActionAdd.vue
+++ src/components/label/LabelActionAdd.vue
@@ -3,25 +3,49 @@
   <v-dialog v-model="dialog" width="500">
     <template v-slot:activator="{ on }">
       <v-btn color="blue-grey" class="white--text" v-on="on">
-        新增主目錄
+        新增目錄
         <v-icon right dark>add_circle</v-icon>
       </v-btn>
     </template>
 
     <v-card>
       <!-- Card Title -->
-      <v-card-title class="headline teal lighten-4" primary-title>新增主目錄名稱</v-card-title>
+      <!-- <v-card-title class="headline teal lighten-4" primary-title>新增主目錄名稱</v-card-title> -->
 
-      <!-- Card Content -->
-      <v-card-text>
-        <v-container grid-list-md>
-          <v-layout wrap>
-            <v-flex>
-              <v-text-field label="主目錄名稱" id="newCatelogName" @keyup.enter="addMainCatelog"></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-text>
+      <v-tabs centered color="cyan" dark icons-and-text>
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+        <v-tab href="#tab-1">
+          主目錄
+          <v-icon>book</v-icon>
+        </v-tab>
+
+        <v-tab href="#tab-2">
+          次目錄
+          <v-icon>bookmarks</v-icon>
+        </v-tab>
+
+        <v-tab href="#tab-3">
+          標籤
+          <v-icon>bookmark</v-icon>
+        </v-tab>
+
+        <!-- Main Catelog -->
+        <v-tab-item value='tab-1'>
+          <v-card flat>
+            <!-- Card Content -->
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex>
+                    <v-text-field label="主目錄名稱" id="newCatelogName" @keyup.enter="addMainCatelog"></v-text-field>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
 
       <v-divider/>
 
@@ -40,13 +64,18 @@
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+      flag: ''
     };
   },
   methods: {
     addMainCatelog() {
-      this.dialog = false;
-      this.$store.dispatch("add_main_catelog", newCatelogName.value);
+      if (newCatelogName.value) {
+        this.dialog = false;
+        this.$store.dispatch("add_main_catelog", newCatelogName.value);
+      }else {
+        alert("請輸入資料");
+      }
     }
   }
 };
